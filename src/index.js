ca36c018d1a63fedff87b0291c7e08cf4196fcc2
Alina Bovdyr <alinabovdyr92@gmail.com>
2020-10-21 21:56:14 +0300

    Добавляет разметку меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import menuMarkup from './templates/menu-card.hbs'
+import menuData from './menu.json'
 import './styles.css';
+
+const menuCardsList = document.querySelector('.js-menu');
+const menuCardsMarkup = createMenuCardsMarkup(menuData);
+
+menuCardsList.insertAdjacentHTML('afterbegin', menuCardsMarkup);
+
+function createMenuCardsMarkup(data) {
+    return data.map(menuMarkup).join('');
+}
